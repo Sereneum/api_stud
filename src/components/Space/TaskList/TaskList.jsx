f8a7153c903d3994db899d4b33798f16a65475f1
Sereneum <maks.rom.111@mail.ru>
2023-03-10 23:56:44 +0300

    cursor: pointer

--- src/components/Space/TaskList/TaskList.jsx
+++ src/components/Space/TaskList/TaskList.jsx
@@ -10,12 +10,16 @@ const TaskList = observer(({course}) => {
         <div className={styles.tasks_block}>
             {course.courses.length
                 ?
-                <div className={styles.task_list}>
-                    {
-                        course.courses[course.activeCourse].tasks.map((i, index) => <Task key={`t${index}`}
-                                                                                          index={index + 1} task={i}
-                                                                                          size={course.courses[course.activeCourse].tasks.length}/>)}
-                </div>
+                <>
+                    <div
+                        className={styles.title}>{course.courses[course.activeCourse].course.course_name}</div>
+                    <div className={styles.task_list}>
+                        {
+                            course.courses[course.activeCourse].tasks.map((i, index) => <Task key={`t${index}`}
+                                                                                              index={index + 1} task={i}
+                                                                                              size={course.courses[course.activeCourse].tasks.length}/>)}
+                    </div>
+                </>
                 :
                 <div className={styles.task_not_found}>
                     <p>Не найдены активные курсы</p>
