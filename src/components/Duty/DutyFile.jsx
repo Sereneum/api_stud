f93a44344f2fc98d329fe91e2f657f65f74f7e32
Sereneum <maks.rom.111@mail.ru>
2023-03-23 14:50:58 +0300

    sisi

--- src/components/Duty/DutyFile.jsx
+++ src/components/Duty/DutyFile.jsx
@@ -5,6 +5,8 @@ import styles from './DutyMaterials.module.css'
 import download_icon from '../../resources/duty/download.svg'
 import preview_icon from '../../resources/duty/view.svg'
 import pdf_file from '../../resources/duty/pdf_file.svg'
+import link_icon from '../../resources/duty/link_icon.svg'
+import link_open from '../../resources/duty/link_open.svg'
 import excel_file from '../../resources/duty/excel_file.svg'
 import pptx_file from '../../resources/duty/pptx_file.svg'
 import picture_file from '../../resources/duty/picture_file.svg'
@@ -20,41 +22,32 @@ const DutyFile = ({file}) => {
     }
 
     const handleDownload = () => {
-        let url = `https://stud.mgri.ru${file.link}`
-        if(url.slice(url.lastIndexOf('.') + 1) == 'pdf')
+        let url = file.nameFile ? `https://stud.mgri.ru${file.link}` : file.link
+        if (url.slice(url.lastIndexOf('.') + 1) == 'pdf')
             window.open(url, '_blank')
         else
-            window.location.href = url
-
-
-        // if(isPdf) window.open(pdfUrl, '_blank');
-        // else window.location.href = `https://stud.mgri.ru${file.link}`
+            if(file.nameFile)
+                window.location.href = url
+            else
+                window.open(url)
     };
 
 
     return (
         <div className={styles.duty_materials_item}>
             <img alt="" src={pdf_file} className={styles.duty_materials_icon}/>
-            <div className={styles.duty_materials_file_name}>{file.nameFile}</div>
+            <div className={styles.duty_materials_file_name}>
+                {file.nameFile ? file.nameFile : file.nameLink}
+            </div>
             <img alt="" src={preview_icon} className={styles.duty_materials_preview_button}/>
-            {/*<a href={`https://stud.mgri.ru${file.link}`} download>*/}
-                <img
-                    alt=""
-                    src={download_icon}
-                    className={styles.duty_materials_download_button}
-                    onClick={handleDownload}
-                />
-            {/*</a>*/}
-
+            <img
+                alt=""
+                src={file.nameFile ? download_icon : link_open}
+                className={styles.duty_materials_download_button}
+                onClick={handleDownload}
+            />
         </div>
     );
 };
 
 export default DutyFile;
-
-{/*<a href={`https://stud.mgri.ru${file.link}`} download>*/
-}
-{/*    {file.nameFile}*/
-}
-{/*</a>*/
-}
